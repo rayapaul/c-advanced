1711d4bb3494261a646e7f55ad58d48152b9288f
rayapaul <rayapaul13@gmail.com>
2021-08-08 14:01:26 +0530

    new repo created

---
+++ delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public delegate void HelloFunctionDelegate(string Message);
+namespace delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+            del("hello from delegate");
+            Hello("hi another way of declaring a delegate");
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
---
+++ generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace generics
+{
+    public class Calculator
+    {
+        public static bool AreEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool Equal = Calculator.AreEqual<string>("AB", "AB");
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not equal");
+            }
+        }
+    }
+}
